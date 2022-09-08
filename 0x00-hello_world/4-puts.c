b8c081935aafe70f1a2dcfe39f5338f86892eb7b
Judeitopa <itopajude1@gmail.com>
2022-09-08 15:27:55 -0700

    code prints a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+  * main - Entry point
+ 
+  * Description: 'the program's description '
+  * Return: Always 0 (Success)
+     **/
+
+
 int main(void)
 {
 puts("Progragmming is like building a multilingual puzzle\0");
