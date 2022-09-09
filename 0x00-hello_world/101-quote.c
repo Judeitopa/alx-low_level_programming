4beef5d5ea0cbd3e49e3e979b319b574bd27a7db
Judeitopa <itopajude1@gmail.com>
2022-09-09 07:10:48 -0700

    code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include<unistd.h>
-#include <stdio.h>
 /**
  * main - Entry point
  * Description - 'code print text with printf or puts'
  * Return: Always 1 (Success)
 */
 int main(void)
-{char str1[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19",
-write(STDOUT_FILENO, srtl[], strlen(str1));
+{char str1[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(2, str1, 59);
 return (1);
 }
