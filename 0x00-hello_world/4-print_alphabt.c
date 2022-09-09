a90d5e25921061323b90678705c16a10ed0758b4
Judeitopa <itopajude1@gmail.com>
2022-09-09 12:49:46 -0700

    code prints alphabet except p and e

---
+++ 0x00-hello_world/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: 'code prints the alphabet except q and e'
+ * Return: Alwasys 0 (Success)
+*/
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+if (ch == 'q' || ch == 'e')
+	putchar('\0');
+return (0);
+}
