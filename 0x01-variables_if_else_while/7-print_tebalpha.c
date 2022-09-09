13a602d94bed1166d72755dc4d536428b27d826d
Judeitopa <itopajude1@gmail.com>
2022-09-09 16:00:05 -0700

    code prints alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'code prints alphabet in reverse order'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		ahpla = strrev(alpha);
+		putchar(ahpla);
+				putchar('\n');
+return (0);
+}
