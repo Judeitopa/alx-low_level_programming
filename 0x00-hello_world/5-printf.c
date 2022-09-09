7e3ac9f3bf3d9cd88ded0e3f8bf29bf29effec3a
Judeitopa <itopajude1@gmail.com>
2022-09-09 02:52:06 -0700

    code prints text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * Description - 'code prints a text'
- * Return(0) - Succcess
-*/
+ * main- Entry point
+ * Description: 'code prints a text'
+ * return(0) - always a success
+ */        
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\0");
+return (0);
 }
