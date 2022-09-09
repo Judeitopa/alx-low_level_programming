d4c95896e58b07ec49976c1becb07cd57fb84f60
Judeitopa <itopajude1@gmail.com>
2022-09-09 10:27:05 -0700

    code prints the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Code prints the alphabet'
+* Return: Always 0 (Success) 
+*/
+int main(){
+char alphabet = 'a';
+while (alphabet <= 'z')
+{putchar(alphabet);
+}
+alphabet++;
+return (0);
+}
