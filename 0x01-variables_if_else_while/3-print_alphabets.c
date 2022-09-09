e7f4a33520da1912f228e46dc28af5df581438fb
Judeitopa <itopajude1@gmail.com>
2022-09-09 11:01:58 -0700

    code prints a;phabet in upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: 'code print alphabet in lower and upper case'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char low = 'a';
+	char up = 'A';
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+for (up = 'A'; up <= 'Z'; up++)
+	putchar(up);
+putchar('\n');
+	return (0);
+}
+
