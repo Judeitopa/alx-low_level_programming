1c2f59fee505ce740bea54feb99ec0d40adad7b6
Judeitopa <itopajude1@gmail.com>
2022-09-09 02:41:23 -0700

    script uses printf to print text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description - 'code prints a text'
+ * Return(0) - Succcess
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}
