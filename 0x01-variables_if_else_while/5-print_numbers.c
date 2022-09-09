ebb6e863f36ed7cacb7510659f375aa2ed584237
Judeitopa <itopajude1@gmail.com>
2022-09-09 15:22:35 -0700

    code prints numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'code prints num in base 10'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ int num;
+	for (num = 0; num < 10 ; num++)
+		printf("%d", num);
+return (0);
+}
