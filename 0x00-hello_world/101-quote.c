da49d3f04d2c5f56e5025ea4767430dcb78da379
Judeitopa <itopajude1@gmail.com>
2022-09-09 07:00:45 -0700

    code prints text without printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * Return: Always 1 (Success)
 */
 int main(void)
-{const char x[] = "and that piece of art is useful/" - Dora Korpar, 2015-10-19"
+{const char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 write(STDOUT_FILENO, x, strlen(x)); // Feel free to check the return value.
 return (1);
 }
