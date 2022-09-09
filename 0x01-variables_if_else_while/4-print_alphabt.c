b57d2162326bb56ed35894d8fe35024376599202
Judeitopa <itopajude1@gmail.com>
2022-09-09 15:11:30 -0700

    code print the alphabet without p and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,15 +5,13 @@
  * Return: Always 0 (Success)
 */
 int main(void)
-{ char ch;
-	for (ch = 'a'; ch <= 'z' ; ch++)
-{if (ch != 'e' && ch != 'q')
-{putchar(ch);
-}
-		else
-		{
-		}
+{
+	char ch = 'a';
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	if (ch != 'q' && ch != 'e')
+	{putchar(ch);
 	}
-putchar('\n');
-return (0);
+}
+	return (0);
 }
