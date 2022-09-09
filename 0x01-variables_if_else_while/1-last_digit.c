ec641cd591d149bc25021a5310684c816d1fbfa4
Judeitopa <itopajude1@gmail.com>
2022-09-09 10:02:30 -0700

    code prints a num with condition

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ if (m > 5)
 if (m == 0)
 {printf("Last digit of %d is %d and is 0\n", n, m);
 }
-if (m < 6 && m > 0)
+if (m < 0)
 {printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 return (0);
