f100e32f8d3bd2927aaeb03051e3b02ad39395d1
Judeitopa <itopajude1@gmail.com>
2022-09-09 09:04:55 -0700

    script prints a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,20 +2,22 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- * Description - 'Code prints a random number 
- * Return - Always 0 (success)
+* main - Entry point
+* Description: 'Code prints a random number'
+* Return: Always 0 (success)
 */
 int main(void)
 {int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0) 
-{printf("%d is postive\n", n);}
+if (n > 0)
+{printf("%d is postive\n", n);
+}
 else if (n == 0)
-{printf("%d is Zero\n", n);}
-else if (n < 0) 
-{printf("%d is negative\n", n);}
-/* your code goes there */
+{printf("%d is Zero\n", n);
+}
+else if (n < 0)
+{printf("%d is negative\n", n);
+}
 return (0);
 }
