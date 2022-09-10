413ecb6b2b4e83ee033e5be6b0e9326654724b87
Judeitopa <itopajude1@gmail.com>
2022-09-10 08:42:03 -0700

    code prints num combo in doubles

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,33 +8,33 @@
 int main(void)
 { int num, num1, num2, num3;
 	for (num = '0' ; num <= '9' ; num++)
-	{
+{
 		for (num1 = '0' ; num1 <= '9' ; num1++)
-		{
+{
 			for (num2 = '0' ; num2 <= '9' ; num2++)
-			{
+{
 				for (num3 = '0' ; num3 <= '9' ; num3++)
-				{
+{
 					if ((((num2 + num3) > (num + num1)) &&  num2 >= num) || num < num2)
-					{
+{
 						putchar(num);
 						putchar(num1);
 						putchar(' ');
 						putchar(num2);
 						putchar(num3);
-						if (num + num1 + num2 + num3  == 227 && num != 9)
-					{break;
-					}
-					else
-					{
+					if (num + num1 + num2 + num3  == 227 && num != 9)
+						{break;
+}
+else
+{
 						putchar(',');
 						putchar(' ');
-					}
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
+}
+}
+}
+}
+}
+}
+putchar('\n');
 return (0);
 }
