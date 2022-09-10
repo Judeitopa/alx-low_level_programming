af01d25b581b0dadddb174b6175ee8a8faa0dc76
Judeitopa <itopajude1@gmail.com>
2022-09-10 03:24:43 -0700

    code prints num of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 /**
- * main -Entry point
- * Description: 'code print base 16'
- * Return: Always 0 (Success)
- */
+* main -Entry point
+* Description: 'code print base 16'
+* Return: Always 0 (Success)
+*/
 int main(void)
-{int num;
+{ int main;
 	char alpha;
-	for (num = 0 ; num <= 9 ; num++)
-	{
-		putchar(num % 10 + '0');
-	}
-	for (alpha = 'a' ; alpha <= 'f' ; alpha++)
-	{
-		putchar(alpha);
-	}
+for (num = 0 ; num <= 9 ; num++)
+{
+	putchar(num % 10 + '0');
+}
+for (alpha = 'a' ; alpha <= 'f' ; alpha++)
+{
+	putchar(alpha);
+}
 	putchar('\n');
 return (0);
 }
