0e1c335c7d57953187ff31ac7679c2435fee9583
Judeitopa <itopajude1@gmail.com>
2022-09-10 03:46:02 -0700

    code prints num wit space and commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - Entry Point
+ * Description: 'Code prints num seperated by comma'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ int num;
+for (num = 0 ; num <= 9 ; num++)
+{
+	putchar(num % 10 + '0');
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
