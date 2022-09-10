2af2061a2f7c6b68c9f975ba1ff4fd99fca64ac3
Judeitopa <itopajude1@gmail.com>
2022-09-10 04:45:32 -0700

    code prints comb with spaces and comma

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - Entry point
+ * Description: 'code prints comb of num'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int num;
+	int num1;
+for (num = '0' ; num <= '8' ; num++)
+{
+for (num1 = '1' ; num <= '9' ; num++)
+{
+if (num < num1)
+{
+	putchar(num);
+	putchar(num1);
+
+}
+}
+if (num != '8' && num1 != '9')
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
