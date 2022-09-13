edd6a40247af7e176320882425797531160095a7
Judeitopa <itopajude1@gmail.com>
2022-09-13 15:41:27 -0700

    code prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  * Description: 'code prints _putchar'
  * Return: Always 0 (Success)
  */
 int main(void)
-{ char n[] = "_putchar";
-	putchar('n');
+{ char text[4] = {'_','p','u','t'};
+	putchar(text[4]);
 return (0);
 }
