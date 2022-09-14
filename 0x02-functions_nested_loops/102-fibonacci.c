ad2171fcb951dc84238038605a8e3c3469243e00
Judeitopa <itopajude1@gmail.com>
2022-09-13 20:27:15 -0700

    code prints the first 52 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,11 +4,11 @@
  * Return: Nothing!
  */
 int main(void)
-{int i=0;
-	long j = 1, k =2;
+{int i = 0;
+	long j = 1, k = 2;
 	while (i < 50)
 	{
-		if (i ==0)
+		if (i == 0)
 			printf("%ld", j);
 		else if (i == 1)
 			printf(", %ld", k);
