56e8ed5695ca36805dc221fcd6d764c754e26b8d
Judeitopa <itopajude1@gmail.com>
2022-09-15 08:49:31 -0700

    code debugs and test the output of zero on function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,10 +2,11 @@
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
+ * i: 'the parameter to be tested'
+ * positve_or_negative() - the function initiating the test
  */
-int main(void)
+int main(void positive_or_negative(int i))
 {int i;
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+i = 0;
+positive_or_negative(i);
 }
