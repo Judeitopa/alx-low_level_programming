c1431cebcbc5ddb36f6a3e929bcaf90895e10639
Judeitopa <itopajude1@gmail.com>
2022-09-15 18:41:16 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,16 @@ void more_numbers(void)
 {
 int n;
 int i;
-	for (n = 0; n <= 10; n++)
+for (n = 0; n <= 10; n++)
+{
+	for (i = 0; i <= 14; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		if (i > 9)
 		{
-			if (i > 9)
-			{
-				_putchar((i / 10) + '0');
-			}
-			_putchar((i % 10) + '0');
+			_putchar((i / 10) + '0');
 		}
-		_putchar('\n');
+		_putchar((i % 10) + '0');
 	}
+	_putchar('\n');
+}
 }
