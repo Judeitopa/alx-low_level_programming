74ab0ac8fab315a8c2b00cc5c47601db266734e3
Judeitopa <itopajude1@gmail.com>
2022-09-15 17:47:25 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prints numbers
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+int n;
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
