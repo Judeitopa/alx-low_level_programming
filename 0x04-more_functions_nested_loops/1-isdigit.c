c799c32523f4d4266a096d238270bc22d263da22
Judeitopa <itopajude1@gmail.com>
2022-09-15 17:11:47 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_isdigit - test 0-9
+ *@c: character to be tested
+ *Return: 0 if c is digit, 1 otherwise
+ */
+int _isdigit(int c)
+{
+int digit;
+	for (digit = 0; digit <= 9; digit++)
+	{
+		if (digit == c)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
