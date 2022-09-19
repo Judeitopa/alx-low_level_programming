7fe1be36bf308e9640f6584d6a63b6371222ac7f
Judeitopa <itopajude1@gmail.com>
2022-09-19 16:25:25 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,29 +3,26 @@
 
 /**
  * main - Entry point
- * print_number - prints a number
- * @n: The number to print
- * Return: return 0
+ *Return: return 0
  */
 
 int main(void)
 {
 long number = 612852475143;
 int inc;
-	
-	while (inc++ < number / 2)
+while (inc++ < number / 2)
+{
+	if (number % inc == 0)
+	{
+		number /= 2;
+		continue;
+	}
+	for (inc = 3; inc < number / 2; inc += 2)
 	{
 		if (number % inc == 0)
-		{
-			number /= 2;
-			continue;
-		}
-		for (inc = 3; inc < number / 2; inc += 2)
-		{
-			if (number % inc == 0)
-				number /= inc;
-		}
+			number /= inc;
 	}
-	printf("%d", inc);
-	return (0);
+}
+printf("%ld", number);
+return (0);
 }
