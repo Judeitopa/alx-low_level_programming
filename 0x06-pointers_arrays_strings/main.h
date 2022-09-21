7899f05f4414976e097608e580ed0e36fb206e48
Judeitopa <itopajude1@gmail.com>
2022-09-21 08:19:47 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
+char transform(char x);
 
 #endif /*MAIN_H*/
